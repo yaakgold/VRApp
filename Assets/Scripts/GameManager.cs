21a410b20ed13b16b653836f5de252aabe1465bf
snajera531 <salemnajera@gmail.com>
2021-07-26 13:25:18 -0600

    adding main menu scene and adding buttons

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+public class GameManager : MonoBehaviour
+{
+    //TODO
+    //have the scene switch to the main menu scene
+
+    public void PlayGame()
+    {
+        //switch to game scene
+    }
+
+    public void Credits()
+    {
+        //activate credits panel
+        //deactivate main panel
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
