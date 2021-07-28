b66231ed54a121d18540e1d4f8ebabf9a45d19d8
BadHatHoffman <EHoffman@student.neumont.edu>
2021-07-28 09:13:23 -0600

    lklkj

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+using System.Collections.Generic;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    //TODO
+    //have the scene switch to the main menu scene
+    public GameObject creditsPanel;
+    public GameObject mainMenuPanel;
+
+    public void PlayGame()
+    {
+        //switch to game scene
+        SceneManager.LoadScene(1);
+    }
+
+    public void Credits()
+    {
+        //activate credits panel if not enabled
+            //if enabled, activate main menu
+        if(creditsPanel.activeInHierarchy)
+        {
+            creditsPanel.SetActive(false);
+            mainMenuPanel.SetActive(true);
+        }
+        //deactivate main menu if enabled
+            //if not enabled, activate main menu
+        else if(mainMenuPanel.activeInHierarchy)
+        {
+            creditsPanel.SetActive(true);
+            mainMenuPanel.SetActive(false);
+        }
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
